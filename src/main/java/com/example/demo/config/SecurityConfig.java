85e77fde6544388d2cbe5d083b9576d7e2a43e0b
Daniel <riesen@yahoo.de>
2020-10-23 14:20:48 +0200

    #8 enable spring boot admin

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -11,9 +11,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
-                .authorizeRequests().anyRequest().authenticated()
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/actuator/**")
+                .permitAll()
+                .anyRequest().authenticated()
                 .and()
                 .httpBasic();
     }
